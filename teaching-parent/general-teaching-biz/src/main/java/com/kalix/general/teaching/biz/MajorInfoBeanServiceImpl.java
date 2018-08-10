8d38997d0b886646ba43d239bde69dd6cc69d56d
houqingjie <150091225@qq.com>
2018-08-10 10:48:48 +0800

    新增专业信息后台字段规则校验

--- teaching-parent/general-teaching-biz/src/main/java/com/kalix/general/teaching/biz/MajorInfoBeanServiceImpl.java
+++ teaching-parent/general-teaching-biz/src/main/java/com/kalix/general/teaching/biz/MajorInfoBeanServiceImpl.java
@@ -1,10 +1,16 @@
 package com.kalix.general.teaching.biz;
 
+import com.kalix.framework.core.api.persistence.JsonStatus;
 import com.kalix.framework.core.api.web.model.BaseTreeDTO;
 import com.kalix.framework.core.impl.biz.TreeExtendShiroGenericBizServiceImpl;
+import com.kalix.framework.core.util.Assert;
 import com.kalix.general.teaching.api.biz.IMajorInfoBeanService;
 import com.kalix.general.teaching.api.dao.IMajorInfoBeanDao;
+import com.kalix.general.teaching.entities.GradeBean;
 import com.kalix.general.teaching.entities.MajorInfoBean;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
 
 /**
  * Created by fj on 2017-8-17.
@@ -18,6 +24,44 @@ public class MajorInfoBeanServiceImpl extends TreeExtendShiroGenericBizServiceIm
         super.init(MajorInfoBean.class.getName());
         super.setTreeName(TREE_NAME);
         super.setBeanName(BEAN_NAME);
+    }
+
+    @Override
+    public boolean isDelete(Long entityId, JsonStatus status) {
+        if (dao.get(entityId) == null) {
+            status.setSuccess(false);
+            status.setMsg(BEAN_NAME + "已经被删除！");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isUpdate(MajorInfoBean entity, JsonStatus status) {
+        Assert.notNull(entity, "实体不能为空.");
+        MajorInfoBean majorInfoBean = (MajorInfoBean) entity;
+        List<MajorInfoBean> beans = dao.find("select ob from MajorInfoBean ob where ob.id <> ?1 and (ob.code = ?2 or ob.standardCode = ?3)",
+                majorInfoBean.getId(), majorInfoBean.getCode(), majorInfoBean.getStandardCode());
+        if (beans != null && beans.size() > 0) {
+            status.setSuccess(false);
+            status.setMsg("更新" + BEAN_NAME + "[" + entity.getCode() + "]失败,已经存在！");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isSave(MajorInfoBean entity, JsonStatus status) {
+        Assert.notNull(entity, "实体不能为空.");
+        MajorInfoBean majorInfoBean = (MajorInfoBean) entity;
+        List<MajorInfoBean> beans = dao.find("select ob from MajorInfoBean ob where ob.code = ?1 or ob.standardCode = ?2",
+                majorInfoBean.getCode(), majorInfoBean.getStandardCode());
+        if (beans != null && beans.size() > 0) {
+            status.setSuccess(false);
+            status.setMsg(BEAN_NAME + "[" + entity.getCode() + "]已经存在！");
+            return false;
+        }
+        return true;
     }
 
     @Override
